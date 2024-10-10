a64e102bb54163061ab4751973df604386799751
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-10-10 19:06:57 -0400

    solved with selection sort, no method

--- solutions/selection_sort.js
+++ solutions/selection_sort.js
@@ -1,5 +1,21 @@
 function selectionSort(arr) {
-  for (i = 0; )
+  let sortedArray = [];
+
+  for ( let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for ( let j = minIndex + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }}
+    
+    // Swap the found minimum element with the element at index i
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];  // Swap elements
+    }
+  }
+  
+  return arr;  // The array is now sorted
 }
 
 // this function uses the sort method to sort
